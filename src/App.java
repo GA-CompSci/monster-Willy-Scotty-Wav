2d41a7585695808436fda09b6dd676162038f686
Willy-Scotty-Wav <scottw@gilmour.org>
2025-09-22 10:35:38 -0400

    monster

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class App {
+    // calss variable
+    private static Monster[] monsters;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("*MONSTER BATTLE*");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many Monsters will you take on: ");
+        int num = input.nextInt(); // todo handle non number crash
+        Monster[] monster = new Monster[num];
+        // build all monsters
+        for(int i = 0; i < monster.length; i ++){
+            monster[i] = new Monster(); // todo add specials
+        }
+        System.out.println(count + " monsters have more thatn 50 health");
+    }
+    /**
+     * how many monsters have over the given health
+     * @param health number to check
+     * @return number of monsters above that health
+     */
+    public static int monsterCount(int healt, Monster[] arr){
+  // how many monsters have more than 50 health
+        int count = 0;
+        // traverse the monsters array and find out how many have < 50 health
+        for(Monster m : monsters){
+           if (m.health() > health) count ++; 
+        }
+        return count;
+    }
+    public static Monster getNextMonster(){
+
+        for(int i = 0; i, monsters.lenght; i++){
+        if(monsters{i}.health() > 0) return monsters[i];
+        }
+        return null;
     }
 }
---
+++ src/Monster.java
@@ -0,0 +1,31 @@
+public class Monster {
+    // instance variables (properties)
+    private int health;
+    private double damage; 
+    private int speed; 
+    private String special;
+
+    // constructor
+    public Monster(){
+        // random generator for health, damage, and speed
+        health = (int)(Math.random() * 81) + 20;
+        // random 10-50
+        damage = (Math.random() * 41) + 10;
+        //speed 1-10
+        speed = (int)(Math.random() * 10) + 1;
+        // by deful the moster doesnt have special move
+        special = "";
+    }
+    // overloaded constructor
+    public Monster(String special){
+        this();
+        this.special = special;
+    }
+
+    // accessor methods
+    public int health () { return this.health;}
+    public double dammage() {return this.damage;}
+    public int speed () {return this.speed;}
+    public String special() {return this.special;}
+
+}
